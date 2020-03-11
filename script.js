5ff8e64477e9ac94745e33086531a2a3f96e8eb9
Brandon1234 <brandonlit@gmail.com>
2020-03-11 15:33:09 -0400

    Updated site
    --added resume page
    --changed the styling
    --other small changes I forgot to push a while ago

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ window.onload = function(){
     
     var btnWork = document.getElementById("btnWorkTerm");
     var btnBack = document.getElementById("btnBack");
+    var btnResume = document.getElementById("btnResume");
     
     btnWork.onclick = function(){
         location.href = "reportPage.html";
     }
     
+    btnResume.onclick = function(){
+        location.href = "resume.html";
+    }
+    
     btnBack.onclick = function(){
-        location.href = "mainPortal.html"
+        location.href = "mainPortal.html";
     }
 }
